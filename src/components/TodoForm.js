9027a236b4755905a05843c6e921dea485b98fb4
pploszaj <103064256+pploszaj@users.noreply.github.com>
2023-03-15 11:56:29 -0400

    add radio buttons

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -33,6 +33,25 @@ function TodoForm(props) {
       <button type='submit'className="button">
         <PlusIcon className="icon"></PlusIcon>
       </button>
+
+      <div class="work">
+        <input type="radio" name="select" id="option-1"></input>
+        <label for="option-1" class="option-1">
+          <span>Work</span>
+        </label>
+      </div>
+      <div className="home">
+        <input type="radio" name="select" id="option-2"></input>
+        <label for="option-2" class="option-2">
+          <span>Home</span>
+        </label>
+      </div>
+      <div className="personal">
+        <input type="radio" name="select" id="option-3"></input>
+        <label for="option-3" class="option-3">
+          <span>Personal</span>
+        </label>
+      </div>
     </form>
   );
 }
